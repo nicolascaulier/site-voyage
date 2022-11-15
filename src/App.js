3e85c7dfac7e5b39ff0ad01e686af285b2135b60
unknown <nicolas@whatson.io>
2022-11-15 18:03:43 +0100

    destinations

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer  from "./components/Footer";
+import Destination from "./components/Destination";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero />
+      <Destination />
+      <Search />
       <Footer />
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import {Cursor, useTypewriter } from 'react-simple-typewriter'
 const Hero = () => {
     const [text, count] = useTypewriter({
         words: [
-            "Salut, c est nico",
+            "HELLO A TOUS, c est Nico",
             "Bienvenue sur mon site ",
             "test pour voir si c est good ",
             "EZ Typewriter ",
@@ -21,15 +21,15 @@ const Hero = () => {
         <div className='absolute w-full h-full top-0 left-0  bg-gray-900/30'></div>
         <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
             <h1> Voyage En Première Classe</h1>
-          <div><h2>{text}</h2></div>  
-            <h2>Top des Locations </h2>
-            <h2>https://www.youtube.com/watch?v=gXdHvoWvViQ a 42:53 min</h2>
-            <form>
+          <div><h2 className='py-4'>{text}</h2></div>  
+            <h2 className='py-4'>Top des Locations </h2>
+            <h2 className='py-4'>https://www.youtube.com/watch?v=gXdHvoWvViQ a 1:10:56 min</h2>
+            <form className='flex justify-between items-center max-v-[700px] mx-auto w-full border p-1 rounded-md text-black bg-gray-100/90'>
                 <div>
-                    <input type="text" placeholder='Rechercher Destinations'/>
+                    <input  className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none' type="text" placeholder='Rechercher Destinations'/>
                 </div>
                 <div>
-                    <button><AiOutlineSearch size={20} className='icon'/></button>
+                    <button><AiOutlineSearch size={20} className='icon' style={{color: '#ffffff'}}/></button>
                 </div>
             </form>
         </div>
